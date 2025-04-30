8076bdb4381e2aaacce403d0528c7b66ad395e34
00Fjongl <65314359+00Fjongl@users.noreply.github.com>
2025-04-30 16:48:26 -0500

    feat: include an example of the parametrize feature in unit testing

--- tests/test_property.py
+++ tests/test_property.py
@@ -1,1 +1,2 @@
 import pytest
+# Not necessary since this is for bonus points
--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -1,1 +1,18 @@
 import pytest
+from src.tasks import search_tasks
+
+@pytest.mark.parametrize("term,expected_matches", [
+    ("coffee", 1),
+    ("Food", 1),
+    ("work", 2),
+    ("fee", 1),
+    ("description", 0)
+])
+def test_search_advanced(term, expected_matches):
+    task1 = {"id": 1, "title": "Brew Coffee for Work", "description": "Brew some coffee for tomorrow morning before heading off."}
+    task2 = {"id": 2, "title": "Prepare Lunch", "description": "Decide what food you'll make to bring to work tomorrow."}
+    task3 = {"id": 3, "title": "Test", "description": "Fee fie foe fum sample text."}
+    tasks = [task1, task2, task3]
+    filtered_tasks = search_tasks(tasks, term)
+    matches = len(filtered_tasks)
+    assert matches == expected_matches
